3d8a65736385699665f5a96933b1c867cb6f1a66
eycanny <nancy.the.coder@gmail.com>
2022-03-22 17:27:31 -0700

    Added comments explaining what I think each line of code is doing

--- sales_report.py
+++ sales_report.py
@@ -1,25 +1,38 @@
+#docstring that tells you what this file should do
 """Generate sales report showing total melons each salesperson sold."""
 
-
+#create an empty list and assign to variable name salespeople
 salespeople = []
+#create an empty list and assign to variable name melons_sold
 melons_sold = []
 
+#opens a file inputted as a string and assign to variable name f
 f = open('sales-report.txt')
 
+#iterate for each line in previously defined opened file
 for line in f:
+    #remove whitespace from the right
     line = line.rstrip()
+    #create a list of items, where items are defined by | as the separator
     entries = line.split('|')
+    #isolate name from list
     salesperson = entries[0]
+    #isolate number of melons sold
     melons = int(entries[2])
 
+    #if name already exists in list called salespeople
     if salesperson in salespeople:
+        #find the index of where the name is in the list and assign to variable name position
         position = salespeople.index(salesperson)
+        #update number of melons sold
         melons_sold[position] += melons
-        
+
+    #otherwise, add name to salespeople list and number of melons sold to melons_sold list    
     else:
         salespeople.append(salesperson)
         melons_sold.append(melons)
 
-
+# iterate based on the length of the salespeople list less 1
 for i in range(len(salespeople)):
+    #print a message saying how much each person sold
     print(f'{salespeople[i]} sold {melons_sold[i]} melons')
